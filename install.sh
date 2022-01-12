c7845e479ccdda9833387e1eb4f7f2feea93fb5f
ponix <ponix@protonmail.com>
2022-01-12 18:59:38 +0000

    Resovled issue with current working dir

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-
+#! /bin/bash
+wd=(`pwd`)
+echo "current directory: " $wd
 echo "###############################"
 echo "## New install Setup scripts ##"
 echo "###############################"
@@ -9,14 +11,15 @@ echo "##################"
 echo "## Pulling keys ##"
 echo "##################"
 echo " "
+read -p "Enter Github Username: " username
+curl https://github.com/$username.keys > ~/.ssh/authorized_keys 
 
-curl https://github.com/ponix4k.keys > ~/.ssh/authorized_keys 
-
-sudo apt install vim git snapd mono-complete golang nodejs default-jdk npm -y
+sudo apt install vim git tmux snapd mono-complete golang nodejs default-jdk npm -y
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-
+cd $wd
+echo "Current dir:" `pwd` 
 echo "##################################"
 echo "#### Create Files and Folders ####"
 echo "##################################"
@@ -30,31 +33,31 @@ echo "fi" >> ~/.bashrc
 
 echo "##################################"
 echo "#### Adding Aliases ####"
-echo "##################################"
+echo "#################################"
 echo " "
 
-echo "alias _full_update='sudo apt-get update && sudo apt-get upgrade -y && sudo apt full-upgrade'" >> ~/.bash_aliases
-echo "alias apt='sudo apt'" >> ~/.bash_aliases
-echo "alias c='clear'" >> ~/.bash_aliases
-echo "alias df='df -h'" >> ~/.bash_aliases
-echo "alias egrep='egrep --color=auto'" >> ~/.bash_aliases
-echo "alias fgrep='fgrep --color=auto'" >> ~/.bash_aliases
-echo "alias gh='history | grep'" >> ~/.bash_aliases
-echo "alias grep='grep --color=auto'" >> ~/.bash_aliases
+#echo "alias _full_update='sudo apt-get update && sudo apt-get upgrade -y && sudo apt full-upgrade'" >> ~/.bash_aliases
+#echo "alias apt='sudo apt'" >> ~/.bash_aliases
+#echo "alias c='clear'" >> ~/.bash_aliases
+#echo "alias df='df -h'" >> ~/.bash_aliases
+#echo "alias egrep='egrep --color=auto'" >> ~/.bash_aliases
+#echo "alias fgrep='fgrep --color=auto'" >> ~/.bash_aliases
+#echo "alias gh='history | grep'" >> ~/.bash_aliases
+#echo "alias grep='grep --color=auto'" >> ~/.bash_aliases
 
-echo "alias l='ls -CF'" >> ~/.bash_aliases
-echo "alias la='ls -A'" >> ~/.bash_aliases
-echo "alias ll='ls -latr'" >> ~/.bash_aliases
-echo "alias ls='ls --color=auto'" >> ~/.bash_aliases
+#echo "alias l='ls -CF'" >> ~/.bash_aliases
+#echo "alias la='ls -A'" >> ~/.bash_aliases
+#echo "alias ll='ls -latr'" >> ~/.bash_aliases
+#echo "alias ls='ls --color=auto'" >> ~/.bash_aliases
 
 
-echo "alias myip='curl ipinfo.io/ip'" >> ~/.bash_aliases
-echo "alias rvm-restart='rvm_reload_flag=1 source '~/.rvm/scripts/rvm'\''' " >> ~/.bash_aliases
-echo "alias sl='ls'" >> ~/.bash_aliases
-echo "alias r!="sudo reboot"" >> ~/.bash_aliases
+#echo "alias myip='curl ipinfo.io/ip'" >> ~/.bash_aliases
+#echo "alias rvm-restart='rvm_reload_flag=1 source '~/.rvm/scripts/rvm'\''' " >> ~/.bash_aliases
+#echo "alias sl='ls'" >> ~/.bash_aliases
+#echo "alias r!="sudo reboot"" >> ~/.bash_aliases
 
-echo "alias gstat="git status"" >> ~/.bash_aliases
-echo "alias glog="git log"" >> ~/.bash_aliases
+#echo "alias gstat="git status"" >> ~/.bash_aliases
+#echo "alias glog="git log"" >> ~/.bash_aliases
 
 
 echo "#####################"
@@ -77,4 +80,8 @@ echo "#### Setting up ~/.vimrc ####"
 echo "#############################"
 
 cp ~/.vimrc ~/backups/vimrc.bak
-source ~/.vimrc
+cat $wd/vimrc.txt > ~/.vimrc
+cat $wd/bash_aliases.txt > ~/.bash_aliases
+cat ~/.bash_aliases
+#source ~/.vimrc
+vim +PluginInstall +qall
